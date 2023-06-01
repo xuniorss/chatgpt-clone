116f4ade819743b9a1b908802a88d1ebc5276945
xuniorss <fortunatojunior2020@gmail.com>
2023-06-01 14:03:57 -0300

    ChatPlaceholder

--- src/components/ChatPlaceholder/index.tsx
+++ src/components/ChatPlaceholder/index.tsx
@@ -1,3 +1,65 @@
+import { IconSunTwentyFour } from '../icons'
+
 export const ChatPlaceholder = () => {
-   return <div>...</div>
+   return (
+      <div className="m-5">
+         <h3 className="my-8 text-center text-4xl font-bold">ChatGPT</h3>
+
+         <div className="m-auto mb-8 flex flex-col gap-5 md:max-w-4xl md:flex-row">
+            <div>
+               <div className="mb-3 flex items-center justify-center text-lg">
+                  <IconSunTwentyFour height={24} width={24} className="mr-3" />
+                  Exemplo
+               </div>
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+            </div>
+
+            <div>
+               <div className="mb-3 flex items-center justify-center text-lg">
+                  <IconSunTwentyFour height={24} width={24} className="mr-3" />
+                  Exemplo
+               </div>
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+            </div>
+
+            <div>
+               <div className="mb-3 flex items-center justify-center text-lg">
+                  <IconSunTwentyFour height={24} width={24} className="mr-3" />
+                  Exemplo
+               </div>
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+
+               <div className="mb-3 rounded bg-white/5 p-3 text-center text-sm text-white">
+                  {'Explique o sentido da via em termos simples'}
+               </div>
+            </div>
+         </div>
+      </div>
+   )
 }
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -2,5 +2,6 @@ import IconClose from './IconClose'
 import IconAdd from './IconAdd'
 import IconTrash from './IconTrash'
 import IconMenu from './IconMenu'
+import IconSunTwentyFour from './IconSunTwentyFour'
 
-export { IconClose, IconAdd, IconTrash, IconMenu }
+export { IconClose, IconAdd, IconTrash, IconMenu, IconSunTwentyFour }
