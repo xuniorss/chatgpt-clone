35e3b769c6ceaec5367fba4c1006b60717c7f725
xuniorss <fortunatojunior2020@gmail.com>
2023-06-01 23:10:47 -0300

    finish

--- src/components/ChatArea/index.tsx
+++ src/components/ChatArea/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Chat } from '@/types/Chat'
+import { useEffect, useRef } from 'react'
 import { ChatMessageItem } from '../ChatMessageItem'
 import { ChatMessageLoading } from '../ChatMessageLoading'
 import { ChatPlaceholder } from '../ChatPlaceholder'
@@ -11,8 +12,14 @@ type ChatAreaProps = {
 }
 
 export const ChatArea = ({ chat, loading }: ChatAreaProps) => {
+   const scrollArea = useRef<HTMLDivElement>(null)
+
+   useEffect(() => {
+      scrollArea.current?.scrollTo(0, scrollArea.current?.scrollHeight)
+   }, [loading, chat?.messages.length])
+
    return (
-      <section className="h-0 flex-auto overflow-y-scroll">
+      <section ref={scrollArea} className="h-0 flex-auto overflow-y-scroll">
          {!chat && <ChatPlaceholder />}
          {chat &&
             chat.messages.map((item) => (
